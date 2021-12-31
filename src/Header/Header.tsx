9618857de3fc3c065611bdd51ef2365bfe205ae2
Aleksey <aleksey__tihakov@ukr.net>
2021-12-31 20:08:43 +0200

    startOfProgect

---
+++ src/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Header = ()=>{
+    return (
+        <div>
+            Header
+            <input type={"range"}/>
+        </div>
+    )
+}
+export default Header;
---
+++ src/Tehnologies/Tehnologies.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Tehnologies = ()=>{
+    return (
+        <div>
+
+        </div>
+    )
+}
+export default Tehnologies;
