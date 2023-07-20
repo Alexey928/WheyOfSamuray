aedbd833f99e36461950a20183221fa91d237e52
Alexey928 <aleksey__tihakov@ukr.net>
2023-07-20 23:39:46 +0300

    continue progect 19

--- src/Resduscers/authUserReduser.ts
+++ src/Resduscers/authUserReduser.ts
@@ -5,7 +5,7 @@ export type AuthActionsType =
     | ReturnType<typeof setAuthUserDataAC>
     | ReturnType<typeof setIsRequestProcessingStatusAC>
 
-type UserAuthStateType = typeof UserAuthState
+export type UserAuthStateType = typeof UserAuthState
 
 
 export function authUserReduser( state=UserAuthState,action:AuthActionsType,):UserAuthStateType{
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import style from "./Header.module.css"
+import {useSelector} from "react-redux";
+import {AppRootStateType} from "../../State/reduxStore";
+import {usersStateType} from "../../ActionCreators/usersAC";
+import {UserAuthStateType} from "../../Resduscers/authUserReduser";
+import isAuthed from "../../asets/free-icon-approved-1478873.png"
+import logo from "../../asets/—Pngtree—hand-painted japanese samurai knife_4236036 (1).png"
 type HeaderPropstype = {
     logo:string
 }
 
 const Header = (props:HeaderPropstype)=>{
+    const auth = useSelector<AppRootStateType,UserAuthStateType>(state => state.userAuth);
+    console.log("header");
+
     return (
         <header className={style.header}>
-
-            <img src={props.logo} alt={"fone"}/>
+            <img src={logo} alt={"img"}/>
+            <div className={style.auth}>
+                <img src={isAuthed} alt={"img"}/>
+            </div>
          </header>
     )
 }
